01575e19225708bc5549bd5edba74d7a9a3b3c6c
Paul Sonkoly <sonkoly.pal@gmail.com>
2024-10-24 11:19:34 +0100

    Added track view page to go server

--- cmd/tracks/tracks.go
+++ cmd/tracks/tracks.go
@@ -1,19 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		_, _ = w.Write([]byte("Hello, world"))
-	})
+	mux.HandleFunc("GET /", viewTracks)
 
 	fs := http.FileServer(http.Dir("./static"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fs))
 
-  err := http.ListenAndServe(":9999", mux)
-  if err != nil {
-    panic(err)
-  }
+	err := http.ListenAndServe(":9999", mux)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func viewTracks(w http.ResponseWriter, _ *http.Request) {
+	t, err := template.ParseFiles("ui/html/base.html", "ui/html/track/track.html")
+	if err != nil {
+		panic(err)
+	}
+	t.Execute(w, nil)
 }

3e136613e03f1ca07df6273d432d23478e4eb7b0
Paul Sonkoly <sonkoly.pal@gmail.com>
2024-11-21 13:33:58 +0000

    some user handler tests

--- app/handler/handler_test.go
+++ app/handler/handler_test.go
@@ -4,8 +4,15 @@ import (
 	"database/sql"
 	"net/http"
 	"os"
+	"path/filepath"
+	"runtime"
 	"testing"
 
+	"github.com/paulsonkoly/tracks/app"
+	"github.com/paulsonkoly/tracks/app/handler"
+	"github.com/paulsonkoly/tracks/app/template"
+	"github.com/paulsonkoly/tracks/repository"
+	"github.com/paulsonkoly/tracks/repository/sqlc"
 	"github.com/stretchr/testify/assert"
 	"golang.org/x/net/context"
 )
@@ -26,11 +33,19 @@ func (noSession) Pop(_ context.Context, _ string) any        { return nil }
 func (noSession) RenewToken(_ context.Context) error         { return nil }
 func (noSession) LoadAndSave(next http.Handler) http.Handler { return next }
 
-func recreateDB(t *testing.T) {
+func init() {
+	// templates cannot be picked up if we are not in project root
+	_, filename, _, _ := runtime.Caller(0)
+	err := os.Chdir(filepath.Join(filepath.Dir(filename), "..", ".."))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func withDB(t *testing.T, f func(*sql.DB)) {
+	// connect to postgres cluster
 	db, err := sql.Open("postgres", "user=tracks_test password=1234567 dbname=postgres sslmode=disable")
 	assert.NoError(t, err)
-
-	defer db.Close()
 
 	// drop database if exists
 	_, err = db.Exec("DROP DATABASE IF EXISTS tracks_test")
@@ -39,20 +54,46 @@ func recreateDB(t *testing.T) {
 	// create database
 	_, err = db.Exec("CREATE DATABASE tracks_test")
 	assert.NoError(t, err)
-}
 
-func openDB(t *testing.T) *sql.DB {
-	// drop and create
-	recreateDB(t)
+	// close connection to cluster
+	db.Close()
 
-	db, err := sql.Open("postgres", "user=tracks_test password=1234567 dbname=tracks_test sslmode=disable")
+	// open connection to test db
+	db, err = sql.Open("postgres", "user=tracks_test password=1234567 dbname=tracks_test sslmode=disable")
 	assert.NoError(t, err)
+
+	defer db.Close()
 
 	// load schema file
 	schema, err := os.ReadFile("db/schema.sql")
 	assert.NoError(t, err)
+
 	_, err = db.Exec(string(schema))
 	assert.NoError(t, err)
 
-	return db
+	_, err = db.Exec("SET search_path TO public")
+	assert.NoError(t, err)
+
+	// validate DB connection / schema loaded correctly
+	_, err = db.Exec("SELECT * FROM users")
+	assert.NoError(t, err)
+
+	f(db)
+}
+
+func withApp(t *testing.T, f func(a *app.App)) {
+	withDB(t, func(db *sql.DB) {
+		queries := sqlc.New(db)
+		repo := repository.New(queries, db)
+
+		a := app.New(noLogger{}, &repo, noSession{}, template.New())
+		f(a)
+	})
+}
+
+func withHandler(t *testing.T, f func(h *handler.Handler)) {
+	withApp(t, func(a *app.App) {
+		h := handler.New(a)
+		f(h)
+	})
 }
--- app/handler/user_test.go
+++ app/handler/user_test.go
@@ -1,11 +1,14 @@
 package handler_test
 
 import (
+	"database/sql"
 	"net/http"
-	"os"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/paulsonkoly/tracks/app"
 	"github.com/paulsonkoly/tracks/app/handler"
@@ -16,21 +19,69 @@ import (
 )
 
 func TestViewUserLogin(t *testing.T) {
-	// cd in project root
-	err := os.Chdir("../..")
-	assert.NoError(t, err)
+	withHandler(t, func(h *handler.Handler) {
+		r := httptest.NewRequest(http.MethodGet, "/user/login", nil)
+		w := httptest.NewRecorder()
 
-	db := openDB(t)
-	defer db.Close()
+		h.ViewUserLogin(w, r)
 
-	queries := sqlc.New(db)
-	repo := repository.New(queries, db)
+		assert.Equal(t, http.StatusOK, w.Code)
+		rendersLoginPage(t, w)
+	})
+}
 
-	a := app.New(noLogger{}, &repo, noSession{}, template.New())
-	h := handler.New(a)
-	if assert.HTTPSuccess(t, h.ViewUserLogin, http.MethodGet, "/user/login", nil) {
-		assert.HTTPBodyContains(t, h.ViewUserLogin, http.MethodGet, "/user/login", nil, "Login")
-		assert.HTTPBodyContains(t, h.ViewUserLogin, http.MethodGet, "/user/login", nil, "Username")
-		assert.HTTPBodyContains(t, h.ViewUserLogin, http.MethodGet, "/user/login", nil, "Password")
-	}
+func TestPostUserLoginInvalidCredentials(t *testing.T) {
+	withHandler(t, func(h *handler.Handler) {
+		r := httptest.NewRequest(http.MethodPost, "/user/login", strings.NewReader("Username=admin&Password=adminpass"))
+		r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		w := httptest.NewRecorder()
+
+		h.PostUserLogin(w, r)
+
+		assert.Equal(t, http.StatusOK, w.Code)
+		rendersLoginPage(t, w)
+		showsInvalidCredentials(t, w)
+	})
+}
+
+func TestPostUserLogin(t *testing.T) {
+	withDB(t, func(db *sql.DB) {
+
+		hash, err := bcrypt.GenerateFromPassword([]byte("adminpass"), 12)
+		assert.NoError(t, err)
+
+		_, err = db.Exec("INSERT INTO users (username, hashed_password, created_at) VALUES ('admin', $1, NOW())", hash)
+		assert.NoError(t, err)
+
+		queries := sqlc.New(db)
+		repo := repository.New(queries, db)
+
+		a := app.New(noLogger{}, &repo, noSession{}, template.New())
+		h := handler.New(a)
+
+		r := httptest.NewRequest(http.MethodPost, "/user/login", strings.NewReader("Username=admin&Password=adminpass"))
+		r.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		w := httptest.NewRecorder()
+
+		h.PostUserLogin(w, r)
+
+		redirectsTo(t, w, "/")
+	})
+}
+
+func rendersLoginPage(t *testing.T, w *httptest.ResponseRecorder) {
+	body := w.Body.String()
+	assert.Contains(t, body, "Login")
+	assert.Contains(t, body, "Username")
+	assert.Contains(t, body, "Password")
+}
+
+func showsInvalidCredentials(t *testing.T, w *httptest.ResponseRecorder) {
+	body := w.Body.String()
+	assert.Contains(t, body, "Invalid credentials.")
+}
+
+func redirectsTo(t *testing.T, w *httptest.ResponseRecorder, url string) {
+	assert.Equal(t, http.StatusSeeOther, w.Code)
+	assert.Equal(t, url, w.Result().Header.Get("Location"))
 }
